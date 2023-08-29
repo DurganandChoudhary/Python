9876f4785e0b229d17b85628d0e4a80ac6542cf0
DURGANAND CHOUDHARY <102054803+DurganandChoudhary@users.noreply.github.com>
2023-08-30 00:02:07 +0530

    Add files via upload

---
+++ Demo11.py
@@ -0,0 +1,229 @@
+a = 33
+b = 200
+
+if b > a:
+  print("b is greater than a")
+
+print()
+
+a = 33
+b = 200
+
+if b > a:
+ print("b is greater than a")
+
+print()
+
+a = 33
+b = 33
+
+if b > a :
+  print("b is greater than a")
+elif a == b :
+  print("a and b are equal")
+
+print()
+
+a=33
+b=200
+if b>a:
+  print("b is greater than a")
+elif a==b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+print()
+
+a=200
+b=33
+if b>a:
+  print("b is greater than a")
+else:
+  print("a is greater than b")
+
+############
+
+print()
+
+a=200
+b=33
+if a>b : print("a is greater than b")
+
+print()
+
+a=2
+b=330
+
+print("A") if a>b else print("B")
+
+###########
+
+print()
+
+a = 330
+b = 330
+
+print("A") if a > b else print("=") if a == b else print("B")
+
+#####################
+
+a=200
+b=33
+c=500
+if a>b and c>a:
+  print("Both the conditions are True")
+
+################
+
+
+a=200
+b=33
+c=500
+if a>b or a>c:
+  print("At least one of the condition is True")
+
+##################
+
+a=33
+b=200
+if not a>b:
+  print("a is NOT greater than b")
+
+#######################
+print()
+x=41
+
+if x>10:
+  print("Above ten,")
+  if x>20:
+    print("and also above 20!")
+  else:
+    print("but not above 20")
+
+print()
+#######################
+
+a = 33
+b = 200
+
+if b > a:
+  pass
+
+# having an empty if statement like this, would raise an error without the pass statement
+
+print()
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+######################
+
+i=1
+while i<=10:
+  print(i)
+  i += 1
+###########################
+print()
+##################
+i=1
+while i<20:
+  print(i)
+  i += 1
+
+########################
+print()
+########################
+
+i=1
+while i<6:
+  print(i)
+  if (i==3):
+    break
+  i += 1
+
+####################
+print()
+####################
+
+i=1
+while i<6:
+  print(i)
+  if (i==3):
+    break
+  
+i += 1
+
+print()
+    
+
+
+i = 1
+while i < 6:
+  print(i)
+  if (i == 4):
+    break
+  i += 1
+
+###########################
+print()
+#########################
+
+i=0
+while i<6:
+  i += 1
+  if i==3:
+    continue
+  print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
