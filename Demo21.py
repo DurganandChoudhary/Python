c21a0dc9c3b46d3639e7f1907ab2258194174dea
DURGANAND CHOUDHARY <102054803+DurganandChoudhary@users.noreply.github.com>
2023-09-08 23:52:27 +0530

    Add files via upload

---
+++ Demo21.py
@@ -0,0 +1,280 @@
+import platform
+
+x = platform.system()
+print(x)
+
+print()
+
+
+import platform
+
+x=platform.system()
+print(x)
+
+print()
+
+import platform
+
+x=platform.system()
+print(x)
+
+print()
+
+import platform
+
+x=platform.system()
+print(x)
+
+print()
+
+
+import platform
+
+x=dir(platform)
+print(x)
+
+print()
+
+
+import platform
+x=dir(platform)
+print(x)
+
+print()
+
+
+import datetime
+
+x=datetime.datetime.now()
+print(x)
+
+print()
+
+import datetime
+
+x=datetime.datetime.now()
+print(x)
+
+
+print()
+
+
+import datetime
+x=datetime.datetime.now()
+print(x)
+
+print()
+
+import datetime
+x=datetime.datetime.now()
+
+print(x.year)
+
+print(x.strftime("%A"))
+
+print()
+
+import datetime
+
+x=datetime.datetime.now()
+
+print(x.year)
+print(x.strftime("%A"))
+
+print()
+
+import datetime
+
+x=datetime.datetime.now()
+
+print(x.year)
+print(x.month)
+print(x.strftime("%A"))
+
+
+print()
+
+import datetime
+
+x=datetime.datetime.now()
+print(x.year)
+print(x.strftime("%A"))
+
+print()
+
+
+import datetime
+
+x=datetime.datetime(2023,9,8)
+print(x)
+
+
+print()
+
+import datetime
+
+x=datetime.datetime(2023,9,8)
+print(x.strftime("%B"))
+
+print()
+
+import datetime
+
+x=datetime.datetime.now()
+print(x.strftime("%w"))
+
+
+#################
+
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.strftime("%G"))
+
+
+
+print()
+
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.strftime("%x"))
+
+
+########################################
+
+
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.strftime("%X"))
+
+##################
+
+
+
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.strftime("%C"))
+
+
+print()
+
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.strftime("%c"))
+
+print()
+
+import datetime
+
+x = datetime.datetime(2018, 5, 31)
+
+print(x.strftime("%W"))
+
+print()
+
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.strftime("%U"))
+
+
+print()
+
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.strftime("%j"))
+
+print()
+
+
+
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.strftime("%f"))
+
+
+print()
+
+########################################################
+
+
+x=min(5,50,1)
+y=max(89,78.999)
+
+print(x)
+print(y)
+
+print()
+
+x=abs(-7.56)
+print(x)
+
+
+
+x=pow(2,5)
+print(x)
+
+print()
+
+import math
+
+x=math.sqrt(64)
+print(x)
+
+print()
+
+import math
+x=math.cos(0)
+print(x)
+
+print()
+
+import math
+
+x=math.ceil(1.4)
+y=math.floor(1.4)
+
+print(x)
+print(y)
+
+print()
+
+import math
+
+x=math.pi
+print(x)
+
+print()
+
+import json
+x='{"name" : "Rahul" , "age":20 , "City":"Patna"}'
+y=json.loads(x)
+
+print(y["name"])
+
+
+
+
+
+
+
+
+
+
+
+
+
