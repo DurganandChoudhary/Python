a7a972dfb1392b49dbebc90e5514ba545dec4194
DURGANAND CHOUDHARY <102054803+DurganandChoudhary@users.noreply.github.com>
2023-10-30 00:36:52 +0530

    Add files via upload

--- q2.py
+++ q2.py
@@ -1,11 +1,11 @@
 human_age = int(input('Input a dog age in human years'))
 
 if human_age < 0:
-	print('Age must be positive number')
-	
+    print('Age must be positive number')
+
 elif human_age <= 2:
-	dog_age = human_age * 10.5
+    dog_age = human_age * 10.5
 else:
-	dog_age = 21 + (human_age - 2)*4
+    dog_age = 21 + (human_age - 2)*4
 
 print('The dog age is', dog_age)
